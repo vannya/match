8ae389106a1da42443b3c8a52302f3aa58ed2433
Vannya <van.tabbert@gmail.com>
2018-02-25 14:36:04 -0600

    AddImageModal properly submits to DB.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import * as actions from "./actions";
-import './App.css';
+import AddImageModal from "./components/AddImageModal";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    modalShowing: false
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchUser();
   }
 
-  renderFilterBar(){
-    switch(this.props.oauth){
+  renderFilterBar() {
+    switch (this.props.oauth) {
       case null:
         return;
-      case false: 
+      case false:
         return (
           <div className="filter-bar">
             <a href="/api/googleLogin">SignIn</a>
@@ -21,23 +25,44 @@ class App extends Component {
             <a href="/api/googleLogin">Login</a>
           </div>
         );
-      default: 
-        return (<a href="/api/logout">Logout</a>);
+      default:
+        return (
+          <div className="filter-bar">
+            <button onClick={() => this.toggleModal()}>Add Images</button>
+            <a href="/api/logout">Logout</a>
+          </div>
+        );
     }
+  }
+
+  toggleModal() {
+    this.setState({
+      modalShowing: !this.state.modalShowing
+    });
+  }
+
+  addNewMeme(newMeme) {
+    console.log(newMeme);
+    this.props.addMeme(newMeme);
   }
 
   render() {
     return (
       <div className="App">
         {this.renderFilterBar()}
-        <button onClick={() => this.props.addMeme({link: "www.google.com"})}>Add Images</button>
+        {!!this.state.modalShowing ? (
+          <AddImageModal
+            toggleModal={() => this.toggleModal()}
+            addMeme={this.props.addMeme}
+          />
+        ) : null}
       </div>
     );
   }
 }
 
-function mapStateToProps({oauth}){
-  return {oauth};
+function mapStateToProps({ oauth }) {
+  return { oauth };
 }
 
 export default connect(mapStateToProps, actions)(App);

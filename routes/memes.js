7795d2859a880a825a5ed1bb072a956da8d50259
Vannya <van.tabbert@gmail.com>
2018-02-27 17:50:54 -0600

    changed post request to upsert for tag editing.  corrected spelling error in demo memes.

--- routes/memes.js
+++ routes/memes.js
@@ -3,32 +3,36 @@ const mongoose = require("mongoose");
 const Meme = mongoose.model("memes");
 
 module.exports = app => {
-
-  // Returns all of a user's memes 
+  // Returns all of a user's memes
   app.get("/api/memes", async (req, res) => {
     if (!req.user) {
       return;
-    };
+    }
     const memes = await Meme.find({ _user: req.user.id });
 
     return res.send(memes);
   });
 
-  // Adds Meme
+  // Adds Meme - Blocks Duplicate Memes, but allows for tag editing with new information
   app.post("/api/newMeme", async (req, res) => {
     if (!req.user) {
       return res.status(401).send({ error: "Login required" });
     }
 
-    const currentMeme = await Meme.find({ link: req.body.link, _user: req.user.id });
-    if(!currentMeme.length) {
-      const meme = await new Meme({
-        link: req.body.link,
-        _user: req.user.id,
-        tags: req.body.tags
-      }).save();
-    }
+    const meme = {
+      link: req.body.link,
+      _user: req.user.id,
+      tags: req.body.tags
+    };
 
+    await Meme.findOneAndUpdate(
+      {
+        link: req.body.link,
+        _user: req.user.id
+      },
+      meme,
+      { upsert: true, new: true }
+    );
     res.send(req.user);
   });
 
@@ -44,31 +48,37 @@ module.exports = app => {
   });
 
   // Fetches all distinct tags
-  app.get("/api/tags", async (req, res) => { 
+  app.get("/api/tags", async (req, res) => {
     if (!req.user) {
       return;
-    };
-    const tags = await Meme.find({_user: req.user.id}).distinct("tags", function(err, res) {
-      if (err) return handleError(err);
+    }
+    const tags = await Meme.find({ _user: req.user.id }).distinct(
+      "tags",
+      function(err, res) {
+        if (err) return handleError(err);
 
-      console.assert(Array.isArray(res));
-      return res;
-    });
+        console.assert(Array.isArray(res));
+        return res;
+      }
+    );
     return res.send(tags);
   });
 
   // Searches based on tag choice
   app.get("/api/tags/:tag", async (req, res) => {
-    if(!req.user) {
+    if (!req.user) {
       return;
-    };
+    }
 
-    if(req.params.tag === "all") {
+    if (req.params.tag === "all") {
       const memes = await Meme.find({ _user: req.user.id });
       res.send(memes);
     } else {
-      const memes = await Meme.find({ _user: req.user.id, tags: req.params.tag });
+      const memes = await Meme.find({
+        _user: req.user.id,
+        tags: req.params.tag
+      });
       res.send(memes);
-    };
-  })
+    }
+  });
 };

32fe663b88b33fb00cc35d5865b40d83bd451468
Vannya <van.tabbert@gmail.com>
2018-02-27 23:07:22 -0600

    added logrocket for ux testing

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import LogRocket from "logrocket";
 import reducers from "./reducers";
 import App from "./App";
 import "./stylesheets/main.css";
 
+LogRocket.init('sr59gr/match');
 const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 
 ReactDOM.render(
   <Provider store={store}>

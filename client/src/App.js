e0e2f256c15208efe3b55a33a5ca27930665f17c
Van Tabbert <van.tabbert@gmail.com>
2019-05-26 13:55:04 -0500

    Moved client to 'old' folder and installed CRA v2.  Fixed client side proxy issue.

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,26 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Route, withRouter } from "react-router-dom";
-import * as actions from "./actions";
-import logo from "./stylesheets/assets/logo.png";
-import example from "./stylesheets/assets/example.jpg";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import HeaderContainer from "./components/Header/HeaderContainer";
-import AddEditModalContainer from "./components/AddEditModal/AddEditModalContainer";
-import Landing from "./components/Landing/Landing";
-import MemeBoard from "./components/MemeBoard/MemeBoard";
-import SEO from "./components/Meta/SEO";
-import SignUp from "./components/SignUp/SignUp";
-
-class App extends Component {
-  state = {
-    modalShowing: false,
-    modalType: "add",
-    memeToEdit: "",
-    theme: "main"
-  };
-
-  // Fetches to determine auth status
-  componentDidMount() {
-    this.props.fetchUser();
-  }
-
-  // Toggles the AddEditModal
-  toggleModal(type, meme) {
-    this.setState({
-      modalShowing: !this.state.modalShowing,
-      modalType: type,
-      memeToEdit: meme || null
-    });
-  }
-
-  // Renders the App
-  render() {
-    return (
-      <div
-        className={`theme-${
-          !!this.props.oauth ? this.props.oauth.theme : "main"
-        }`}
-      >
-        <div className="App">
-          <HeaderContainer
-            openAddModal={() => this.toggleModal("add", null)}
-            closeAddModal={() => this.toggleModal(null)}
-          />
-          <div className="app-body">
-            <Route exact path="/" render={() => <Landing logo={logo} example={example} />} />
-            <Route
-              exact path="/memeboard"
-              render={() => (
-                <MemeBoard
-                  toggleModal={() =>
-                    this.toggleModal("edit", this.props.currentMeme)
-                  }
-                  memes={this.props.memes}
-                />
-              )}
-            />
-            <Route exact path="/signup" render={() => <SignUp logo={logo} />} />
-          </div>
-          {!!this.state.modalShowing ? (
-            <AddEditModalContainer
-              meme={this.state.memeToEdit}
-              modalType={this.state.modalType}
-              toggleModal={() => this.toggleModal(null)}
-            />
-          ) : null}
-        </div>
-        <SEO url="default" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-function mapStateToProps({ oauth, memes, tags, currentMeme }) {
-  return { oauth, memes, tags, currentMeme };
-}
-
-export default withRouter(connect(mapStateToProps, actions)(App));
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import LogRocket from "logrocket";
-import reducers from "./reducers";
-import App from "./App";
-import "./stylesheets/main.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-LogRocket.init('tsfljv/match-ufi2i');
-const store = createStore(reducers, {}, applyMiddleware(thunk, LogRocket.reduxMiddleware()));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-ReactDOM.render(
-  <BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
